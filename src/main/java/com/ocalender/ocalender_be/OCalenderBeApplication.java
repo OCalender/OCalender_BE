03e360fb54cba117160c0f78ddac0446f5691a2b
barcode880 <zwonyoung33@gmail.com>
2025-08-20 00:29:46 +0900

    initial commit

---
+++ src/main/java/com/ocalender/ocalender_be/OCalenderBeApplication.java
@@ -0,0 +1,13 @@
+package com.ocalender.ocalender_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OCalenderBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OCalenderBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocalender/ocalender_be/OCalenderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocalender.ocalender_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OCalenderBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
